c7e01350aee6a318acf724af0b20f0bf467cae75
harinikathirvel-31 <harinikathirvel01@gmail.com>
2026-06-10 10:43:54 +0530

    day4.java

---
+++ src/Test4.java
@@ -0,0 +1,21 @@
+void main() {
+    int[][] attendence = {
+            {0, 1, 0},
+            {1, 0, 0},
+            {1, 1, 1,},
+            {1, 0, 1},
+            {0, 0, 0,}
+    };
+    for (int row = 0; row < attendence.length; row++) {
+        for (int col = 0; col < attendence[row].length; col++)
+            if (class) {
+                System.out.print("0=present");
+        }
+        else{
+            System.out.print("present");
+        }
+        System.out.println();
+
+    }
+}
+
